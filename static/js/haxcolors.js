eb7d011a02fcee2ea0e3dbea6d9755a5c42557bf
Francisco Demartino <demartino.francisco@gmail.com>
2014-11-03 21:46:55 -0300

    cleanup a bit

--- static/js/haxcolors.js
+++ static/js/haxcolors.js
@@ -104,24 +104,3 @@ $(function () {
   var clip = new ZeroClipboard( $("button#copy-button") );
   redraw();
 })
-
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-
-(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/{{ fblocale }}/all.js#xfbml=1&appId=561699180553513";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-42549616-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
